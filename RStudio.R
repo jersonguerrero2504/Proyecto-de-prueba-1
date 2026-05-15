38f3198e66dd4f29dc094840ff5dbede3c527c74
Salvatore <20241394@lamolina.edu.pe>
2026-05-15 11:33:18 -0500

    Agregando varios archivos

---
+++ RStudio.R
@@ -0,0 +1,10 @@
+##############################################
+#
+#      Ejemplo para mi proyecto
+#
+##############################################
+
+
+library(readxl)
+archivo = read_xlsx("Regre.xlsx")
+
